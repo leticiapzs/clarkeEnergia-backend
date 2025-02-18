fd4233cd7297769ff38de8193704fdf2d116d27e
Letícia Peixoto <leticia_95lima@hotmail.com>
2025-02-18 10:11:04 -0300

    chore: criação de data para organização de dados

---
+++ src/data/fornecedores.js
@@ -0,0 +1,50 @@
+const fornecedores = [
+    {
+      nome: "Elera Renováveis",
+      logo: "",
+      estadoOrigem: "São Paulo",
+      custoKwh: 0.45,
+      limiteMinKwh: 10000,
+      numeroClientes: 12000,
+      avaliacaoMedia: 4.5
+    },
+    {
+      nome: "Raízen Power",
+      logo: "",
+      estadoOrigem: "São Paulo",
+      custoKwh: 0.50,
+      limiteMinKwh: 15000,
+      numeroClientes: 8000,
+      avaliacaoMedia: 4.8
+    },
+    {
+      nome: "Atvos",
+      logo: "",
+      estadoOrigem: "São Paulo",
+      custoKwh: 0.40,
+      limiteMinKwh: 12000,
+      numeroClientes: 15000,
+      avaliacaoMedia: 4.3
+    },
+    {
+      nome: "FazSol",
+      logo: "",
+      estadoOrigem: "São Paulo",
+      custoKwh: 0.55,
+      limiteMinKwh: 8000,
+      numeroClientes: 2000,
+      avaliacaoMedia: 4.2
+    },
+    {
+      nome: "BGEL (Brasil Geração de Energia Limpa)",
+      logo: "",
+      estadoOrigem: "São Paulo",
+      custoKwh: 0.48,
+      limiteMinKwh: 10000,
+      numeroClientes: 10000,
+      avaliacaoMedia: 4.7
+    }
+  ];
+  
+  module.exports = fornecedores;
+  
